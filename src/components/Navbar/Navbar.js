947d333a603b7488bcf1585d08de9dc1cf390418
Fawzy20 <fawzy-elsam@outlook.fr>
2022-05-09 15:06:47 +0200

    mise en place du design du formulaire de contact

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
         <div className='container-fluid'>
         <div className='row'>
           <div className='col-lg-6 navbar-left'>
-           <a href='https://fawzy-elsam.netlify.app/'><img className='logo' src={logo} width={90} height={90} alt='' /></a>
+           <a href='/'><img className='logo' src={logo} width={90} height={90} alt='' /></a>
           </div>
           <div className='col-lg-6 navbar-right'>
             <Button href='/contact' className='contact' variant="outline-primary">Me contacter</Button>{' '}
