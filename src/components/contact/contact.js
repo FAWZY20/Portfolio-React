6611e291ad4613a2ab3d810fedfa1778ae6f8890
Fawzy20 <fawzy-elsam@outlook.fr>
2022-05-09 14:59:12 +0200

    mise en place du design du formulaire de contact

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,36 +1,44 @@
+import './contact.css';
 import Bitmojie from '../../assets/bitmojie.png';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import { Button } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 
 function FormContact() {
     return (
-        <div>
-            <div>
-                <img className='bitmojie' src={Bitmojie} width={84} height={84} alt='background orinateur ' />
-                <h1>
-                    Merci d'avoir pris le temps de nous contacter. Comment puis-je vous aider aujourd'hui?
-                </h1>
+        <div className='container'>
+            <div className='container-fluid'>
+                <div>
+                    <div className='bitmojie-block' >
+                        <img className='bitmojie' src={Bitmojie} width={84} height={84} alt='background orinateur ' />
+                    </div>
+                    <div>
+                        <h1>
+                            Merci d'avoir pris le temps de nous contacter. Comment puis-je vous aider aujourd'hui?
+                        </h1>
+                    </div>
+                </div>
+                <div className='contact-form'>
+                    <Form>
+                        <Row className="mb-3">
+                            <Form.Group as={Col} controlId="formGridEmail">
+                                <Form.Control type="text" placeholder="Name" />
+                            </Form.Group>
+                            <Form.Group as={Col} controlId="formGridPassword">
+                                <Form.Control type="email" placeholder="Email" />
+                            </Form.Group>
+                        </Row>
+                        <Form.Group class="form-row">
+                            <Form.Control as="textarea" placeholder="Message" rows={3} />
+                        </Form.Group>
+                        <Button className='contact-submit' variant="primary" type="submit">
+                            Envoyer
+                        </Button>
+                    </Form>
+                </div>
             </div>
-            <div>
-                <Form>
-                    <Form.Group class="form-row">
-                        <div class="col-lg-6">
-                            <Form.Control type="text" class="form-control" placeholder="Nom" />
-                        </div>
-                        <div class="col-lg-6">
-                            <Form.Control type="email" class="form-control" placeholder="Email" />
-                        </div>
-                    </Form.Group>
-                    <Form.Group class="form-row">
-                        <Form.Control as="textarea" placeholder="Message" rows={3} />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Envoyer
-                    </Button>
-                </Form>
-            </div>
-
         </div>
     );
 }
