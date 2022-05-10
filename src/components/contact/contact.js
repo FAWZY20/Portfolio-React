834584ee4b17ada983b8d6dd8f20134785dc8b55
Fawzy20 <fawzy-elsam@outlook.fr>
2022-05-10 03:26:58 +0200

    réalisation de l'envoi du formulaire

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -5,8 +5,37 @@ import Form from 'react-bootstrap/Form';
 import { Button } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
+import emailjs from '@emailjs/browser';
+import { useState } from 'react';
+
+
+const Result = () => {
+    return(
+        <div className='sucess-message' >
+            <p>votre message à bien était envoyer !</p>
+        </div>
+    );
+}
 
 function FormContact() {
+    
+    const [result, showResult] = useState(false);
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm("service_2mqx5gp", "template_cv7qg99", e.target, "1x0g9sVHu9hOWIqNZ")
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset();
+          showResult(true);
+    };
+
+    setTimeout(() => {
+        showResult(false)
+    }, 5000)
+
     return (
         <div className='container'>
             <div className='container-fluid'>
@@ -21,21 +50,22 @@ function FormContact() {
                     </div>
                 </div>
                 <div className='contact-form'>
-                    <Form>
+                    <Form action="" onSubmit={sendEmail}>
                         <Row className="mb-3">
                             <Form.Group as={Col} controlId="formGridEmail">
-                                <Form.Control type="text" placeholder="Name" />
+                                <Form.Control type="text" name='name' placeholder="Name" />
                             </Form.Group>
                             <Form.Group as={Col} controlId="formGridPassword">
-                                <Form.Control type="email" placeholder="Email" />
+                                <Form.Control type="email" name='email' placeholder="Email" />
                             </Form.Group>
                         </Row>
                         <Form.Group class="form-row">
-                            <Form.Control as="textarea" placeholder="Message" rows={3} />
+                            <Form.Control as="textarea" name='message' placeholder="Message" rows={3} />
                         </Form.Group>
-                        <Button className='contact-submit' variant="primary" type="submit">
-                            Envoyer
-                        </Button>
+                        <Button className='contact-submit' variant="primary" type="submit">Envoyer</Button>
+                       <div className='row' >
+                            {result ? <Result /> : null}
+                        </div>
                     </Form>
                 </div>
             </div>
