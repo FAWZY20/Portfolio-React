20faed4fc20eb6a966c2743d0da4800cdf3f8fe6
fawzy20 <fawzy-elsam@outlook.fr>
2024-05-27 18:03:19 +0200

    modification des competences

--- src/components/Presentation/Presentation.js
+++ src/components/Presentation/Presentation.js
@@ -26,7 +26,7 @@ function Presentation() {
             </div>
             <div className='container competence-block'>
                 <div className='row'>
-                    <div className='competence col-4'>
+                    <div className='competence col-3'>
                         <div className='front-icon'>
                             <img src={Frontend} width={100} height={100} alt='' />
                             <p>Frontend</p>
@@ -42,7 +42,7 @@ function Presentation() {
                             </ul>
                         </div>
                     </div>
-                    <div className='competence col-4'>
+                    <div className='competence col-3'>
                         <div className='back-icon'>
                             <img src={Backend} width={100} height={100} alt='' />
                             <p>Backend</p>
@@ -61,7 +61,7 @@ function Presentation() {
                             </ul>
                         </div>
                     </div>
-                    <div className='competence col-4'>
+                    <div className='competence col-3'>
                         <div className='back-icon'>
                             <img src={Database} width={100} height={100} alt='' />
                             <p>Database</p>
@@ -78,6 +78,26 @@ function Presentation() {
                                     <li>PhpMyAdmin</li>
                                     <li>PySpark</li>
                                     <li>GraphQL</li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                    <div className='competence col-3'>
+                        <div className='back-icon'>
+                            <img src={Devops} width={100} height={100} alt='' />
+                            <p>Devops</p>
+                        </div>
+                        <div className='text'>
+                            <p className='text-subtitle' >Langues que je parle :</p>
+                            <p>
+                                <ul className='list-competence'>
+                                    <li>Docker</li>
+                                    <li>Jenkins</li>
+                                    <li>Git</li>
+                                    <li>GitLab</li>
+                                    <li>Bash</li>
+                                    <li>Jira</li>
+                                    <li>Confluence</li>
                                 </ul>
                             </p>
                         </div>
